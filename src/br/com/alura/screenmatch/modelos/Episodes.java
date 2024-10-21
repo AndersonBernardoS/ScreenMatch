60f8e23cb160786f9359dd802c33158334ec297c
Anderson Bernardo <anderson.dim.abs@gmail.com>
2024-10-21 19:19:40 -0300

    Initial commit

---
+++ src/br/com/alura/screenmatch/modelos/Episodes.java
@@ -0,0 +1,31 @@
+package br.com.alura.sreennmatch.modelos;
+
+public class Episodes {
+    private int numero;
+    private String nome;
+    private String serie;
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Title.java
@@ -0,0 +1,62 @@
+package br.com.alura.sreennmatch.modelos;
+
+public class Title {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacao;
+    private int duracaoEmMinutos;
+
+    public double mediaAvaliacao() {
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return this.anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean getIncluidoNoPlano() {
+        return this.incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return this.duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacao() {
+        return this.totalDeAvaliacao;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " +nome);
+        System.out.println("Ano de lançamento: " +anoDeLancamento);
+        System.out.println("Duração em minutos: " +duracaoEmMinutos);
+        System.out.println("Incluído no plano: " +incluidoNoPlano);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+}

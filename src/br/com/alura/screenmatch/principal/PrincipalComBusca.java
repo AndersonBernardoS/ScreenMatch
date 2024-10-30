006ab9e81dcfe1ccb7e971f4febd529012fee607
Anderson Bernardo <anderson.dim.abs@gmail.com>
2024-10-30 16:05:13 -0300

    Fix comandos de try e catch no PrincipalComBusca.java

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,49 +6,69 @@ import br.com.alura.screenmatch.modelos.TituloOmdb;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite o nome de um filme ou série: ");
-        var busca = leitura.nextLine();
+        String busca = "";
 
-        String chave = "13437db9";
-        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&plot=full&apikey=" + chave;
+        List<Title> titulos = new ArrayList<>();
 
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
-            String json = response.body();
-            System.out.println(json);
+        while (!busca.equalsIgnoreCase("sair")) {
+            System.out.println("Digite o nome de um filme ou série: ");
+            busca = leitura.nextLine();
 
+            if(busca.equalsIgnoreCase("sair")) {
+                break;
+            }
 
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-            TituloOmdb myTitleOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(myTitleOmdb);
+            String chave = "13437db9";
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&plot=full&apikey=" + chave;
 
-            Title myTitle = new Title(myTitleOmdb);
-            System.out.println(myTitle);
-        } catch (NumberFormatException e) {
-            System.out.println("Ocorreu um erro: " + e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ocorreu um erro no link: " + e.getMessage());
-        } catch (ErroDeConversaoDeAnoException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("O programa finalizou corretamente!");
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb myTitleOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(myTitleOmdb);
+
+                Title myTitle = new Title(myTitleOmdb);
+                System.out.println(myTitle);
+
+                titulos.add(myTitle);
+            } catch (NumberFormatException e) {
+                System.out.println("Ocorreu um erro: " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ocorreu um erro no link: " + e.getMessage());
+            } catch (ErroDeConversaoDeAnoException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.println("O programa finalizou corretamente!");
+                System.out.println(titulos);
+            }
         }
+        FileWriter escrita = new FileWriter("filmes.txt");
+        escrita.write(gson.toJson(titulos));
     }
 }
